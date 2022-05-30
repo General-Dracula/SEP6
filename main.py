9e3638a0cc0e9514e9017b4314f774ccd06bed7d
Dobre-Mircea <dobremirceaionut@gmail.com>
2022-05-30 13:46:47 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -6,28 +6,22 @@ app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 
 @app.route("/")
 def main_app():
-  user_visitted_page() #logs into the database when a user logs in, deletes logs older than 26 hours
-  print('Users visited in the last hour: ' + str(get_visit_numbers_last_24hr()))
-  add_user(random_char(7)+"@gmail.com", 'sugaciul99') #add an user to the dbs with a random email SHOULD CHECK EMAIL CORRECTEBILITY AND PASSWORD OR IF ALLREADY MADE
-  print(add_user('muila@gmail.com', 'sugaciul99')) #add an user to the dbs with a random email SHOULD CHECK EMAIL CORRECTEBILITY AND PASSWORD OR IF ALLREADY MADE
-  add_favorite_movie('69', 'muila@gmail.com')
-  add_favorite_movie('70', 'muila@gmail.com')
-  add_favorite_movie('71', 'muila@gmail.com')
-  add_favorite_movie('72', 'muila@gmail.com')
+  user_visitted_page() #DO NOT COMMENT
+  #logs into the database when a user logs in, deletes logs older than 26 hours
   
-  print('Muila found at: ' + str(get_user_id("muila@gmail.com"))) 
-  #print(get_user_id("muila@gmail.com")) works
-  
-  #delete_user("muila@gmail.com") #works
-  
+  #print('Users visited in the last hour: ' + str(get_visit_numbers_last_24hr()))
+  #add_user(random_char(7)+"@gmail.com", 'sugaciul99') #add an user to the dbs with a random email SHOULD CHECK EMAIL CORRECTEBILITY AND PASSWORD OR IF ALLREADY MADE
+  #print(add_user('muila@gmail.com', 'sugaciul99')) #add an user to the dbs with a random email SHOULD CHECK EMAIL CORRECTEBILITY AND PASSWORD OR IF ALLREADY MADE
+  #add_favorite_movie('69', 'muila@gmail.com')
+  #print('Muila found at: ' + str(get_user_id("muila@gmail.com"))) 
+  #print(get_user_id("muila@gmail.com")) works 
+  #delete_user("muila@gmail.com") #works 
   #delete_favorite_movie('69', "muila@gmail.com")
-  
-  print(get_favorite_movies("muila@gmail.com"))
-  
-  if(check_password('muila@gmail.com', 'sugaciul99') == TRUE): # try password sugaciul99
-    print("------Correct password")
-  else: 
-    print("------False password or not made")
+  #print(get_favorite_movies("muila@gmail.com"))
+  #if(check_password('muila@gmail.com', 'sugaciul99') == TRUE): # try password sugaciul99
+  #  print("------Correct password")
+  #else: 
+  #  print("------False password or not made")
     
   return send_from_directory(app.static_folder, 'index.html')
 
